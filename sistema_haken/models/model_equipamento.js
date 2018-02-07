b510efef3ad52675a8691dcbc06051e6242ca192
Gabrields1998 <gabrieldavidsacca@hotmail.com>
2018-02-07 19:58:21 -0200

    modelo do equipamento

--- sistema_haken/models/model_equipamento.js
+++ sistema_haken/models/model_equipamento.js
@@ -1,47 +1,63 @@
 
-var equipamento = function(identificador, dataCompra, descricao, status, campusOrigem, login){
+function equipamento (identificador, dataCompra, descricao, status, campusOrigem, login){
+	var self = this;
+	this.identificadorLocal = identificador;
+	this.dataCompraLocal = dataCompra;
+	this.descricaoLocal = descricao;
+	this.statusLocal = status;
+	this.campusOrigemLocal = campusOrigem;
+	this.loginLocal = login;
 
-	var identificadorLocal = identificador;
-	var dataCompraLocal = dataCompra;
-	var descricaoLocal = descricao;
-	var statusLocal = status;
-	var campusOrigemLocal = campusOrigem;
-	var loginLocal = login;
-
-	this.getIdentificador = function(){
-		return identificadorLocal;
-	}
-	this.getDataCompra = function(){
-		return dataCompraLocal;
-	}
-	this.getDescricao = function(){
-		return descricaoLocal;
-	}
-	this.getStatus = function(){
-		return statusLocal;
-	}
-	this.getCampusOrigem = function(){
-		return campusOrigemLocal;
-	}
-	this.getLogin = function(){
-		return loginLocal;
-	}
-	this.setIdentificador = function(newIdentificador){
-		identificadorLocal = newIdentificador;
-	}
-	this.setDataCompra = function(newDataCompra){
-		dataCompraLocal = newDataCompra;
-	}
-	this.setDescricao = function(newDescricao){
-		descricaoLocal = newDescricao;
-	}
-	this.setStatus = function(newStatus){
-		statusLocal = newStatus;
-	}
-	this.setCampusOrigem = function(newCampusOrigem){
-		campusOrigemLocal = newCampusOrigem;
-	}
-	this.setLogin = function(newLogin){
-		loginLocal = newLogin;
+	return{
+		getIdentificador : function(){
+			return self.identificadorLocal;
+		},
+		getDataCompra : function(){
+			return self.dataCompraLocal;
+		},
+		getDescricao : function(){
+			return self.descricaoLocal;
+		},
+		getStatus : function(){
+			return self.statusLocal;
+		},
+		getCampusOrigem : function(){
+			return self.campusOrigemLocal;
+		},
+		getLogin : function(){
+			return self.loginLocal;
+		},
+		setIdentificador : function(newIdentificador){
+			self.identificadorLocal = newIdentificador;
+		},
+		setDataCompra : function(newDataCompra){
+			self.dataCompraLocal = newDataCompra;
+		},
+		setDescricao : function(newDescricao){
+			self.descricaoLocal = newDescricao;
+		},
+		setStatus : function(newStatus){
+			self.statusLocal = newStatus;
+		},
+		setCampusOrigem : function(newCampusOrigem){
+			self.campusOrigemLocal = newCampusOrigem;
+		},
+		setLogin : function(newLogin){
+			self.loginLocal = newLogin;
+		}
 	}
 };
+
+/*------------TESTE----------------*/
+
+
+// var equipamento = new equipamento('1', '1999/1/1', 'exemplo', 'Ocupado', 'Campo Mourao', '123456');
+// console.log(equipamento.getIdentificador());
+// console.log(equipamento.getDataCompra());
+// console.log(equipamento.getDescricao());
+// console.log(equipamento.getStatus());
+// console.log(equipamento.getCampusOrigem());
+// console.log(equipamento.getLogin());
+
+// equipamento.setLogin('654321');
+// console.log(equipamento.getLogin());

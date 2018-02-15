183a1d04a69e70489e50cdec86332b90481ac354
vitorCamargo <vitorcamargo@alunos.utfpr.edu.br>
2018-02-14 23:56:38 -0200

    Implementação Tela Pesquisa

--- sistema_haken/public/javascripts/demos/ui-notifications.js
+++ sistema_haken/public/javascripts/demos/ui-notifications.js
@@ -1,16 +1,29 @@
 $(function () {
 
-  $('.howler').click (function (e) {
+  $('.howlerSuccess').click (function (e) {
 
     $.howl ({
       type: $(this).data ('type')
-      , title: 'Howl Message'
-      , content: 'Lorem ipsum dolor sit amet, consect adipisicing elit.'
+      , title: 'Exclusão realizada com Sucesso'
+      , content: 'Atualize a página para vizualizar os campos atualizados.'
       , sticky: $(this).data ('sticky')
       , lifetime: 7500
       , iconCls: $(this).data ('icon')
     });
 
   });
-    
+
+  $('.howlerFail').click (function (e) {
+
+    $.howl ({
+      type: $(this).data ('type')
+      , title: 'Não foi possível realizar essa Exclusão'
+      , content: 'Por favor tente de novo, caso o erro persista, entre em contato.'
+      , sticky: $(this).data ('sticky')
+      , lifetime: 7500
+      , iconCls: $(this).data ('icon')
+    });
+
+  });
+
 });

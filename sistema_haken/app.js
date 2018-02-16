44af219bd61a7109d77ff8cd0f79ede8414c6302
vitorCamargo <vitorcamargo@alunos.utfpr.edu.br>
2018-02-16 16:39:45 -0200

    Atualização do front
    Atualização das páginas já criadas para funcionarem pelo servidor node

--- sistema_haken/app.js
+++ sistema_haken/app.js
@@ -14,6 +14,61 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//GET página inicial
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+//GET página pesquisa
+app.get('/pesquisa', function(req, res) {
+    res.render('pesquisa');
+});
+
+//GET página cadastro equipamento
+app.get('/cad_equipamento', function(req, res) {
+    res.render('cad_equipamento');
+});
+
+//GET página cadastro sala
+app.get('/cad_sala', function(req, res) {
+    res.render('cad_sala');
+});
+
+//GET página movimentação
+app.get('/movimentacao', function(req, res) {
+    res.render('movimentacao');
+});
+
+//GET página empréstimos
+app.get('/emprestimos', function(req, res) {
+    res.render('emprestimos');
+});
+
+//GET página relatório
+app.get('/relatorio', function(req, res) {
+    res.render('relatorio');
+});
+
+//GET página histórico
+app.get('/historico', function(req, res) {
+    res.render('historico');
+});
+
+//GET página cadastro
+app.get('/cadastro', function(req, res) {
+    res.render('cadastro');
+});
+
+//GET página login
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+//GET página esqueceu senha
+app.get('/esqueceu_senha', function(req, res) {
+    res.render('esqueceu_senha');
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -38,9 +93,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // renderiza a página erro
   res.status(err.status || 500);
-  res.render('error');
+  res.render('page-500');
 });
 
 module.exports = app;

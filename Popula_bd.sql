0b99df30fffad1a53b87f168b96a6825f044980a
Gabrields1998 <gabrieldavidsacca@hotmail.com>
2018-03-01 11:59:56 -0300

    Popular Tabelas

--- Popula_bd.sql
+++ Popula_bd.sql
@@ -1,3 +1,5 @@
+
+/*Popular Tabelas Teste*/
 INSERT INTO `sistema_haken`.`equipamento` (`identificador`, `data_compra`, `descricao`, `status`, `campus_origem`, `login`) VALUES ('123', '2018-01-01', 'teste', '1', 'Campo Mourao', '1921860');
 INSERT INTO `sistema_haken`.`equipamento` (`identificador`, `data_compra`, `descricao`, `status`, `campus_origem`, `login`) VALUES ('321', '2018-02-01', 'teste1', '0', 'Campo Mourao', '1921924');
 
